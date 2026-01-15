1ec35eb060178d68dbf302b9b59e5ad9c328d001
Naufal Alief <naufalalief087@gmail.com>
2026-01-15 23:11:17 +0700

    fix: correct SQL syntax by removing extraneous comma in banner insert statement

--- src/schema/banner.sql
+++ src/schema/banner.sql
@@ -8,6 +8,6 @@ CREATE TABLE banner (
 INSERT INTO banner (banner_name, banner_image, description) VALUES
 ('Banner 1', 'https://minio.nutech-integrasi.com/take-home-test/banner/Banner-1.png', 'Lerem Ipsum Dolor sit amet'),
 ('Banner 2', 'https://minio.nutech-integrasi.com/take-home-test/banner/Banner-2.png', 'Banner kedua untuk testing'),
-('Banner 3', 'https://minio.nutech-integrasi.com/take-home-test/banner/Banner-3.png', 'Banner ketiga contoh data');,
+('Banner 3', 'https://minio.nutech-integrasi.com/take-home-test/banner/Banner-3.png', 'Banner ketiga contoh data'),
 ('Banner 4', 'https://minio.nutech-integrasi.com/take-home-test/banner/Banner-4.png', 'Banner keempat untuk demo'),
 ('Banner 5', 'https://minio.nutech-integrasi.com/take-home-test/banner/Banner-5.png', 'Contoh banner kelima');
